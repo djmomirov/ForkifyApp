c83d7382007cda077febc9a09ae793de8bfcb87f
djordje <dj.momirov@gmail.com>
2022-07-25 12:42:02 +0200

    Refactored views

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,14 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import searchView from './views/searchView.js';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 // const recipeContainer = document.querySelector('.recipe');
 
@@ -30,6 +35,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     //1)Get search query
     const query = searchView.getQuery();
 
@@ -38,13 +45,12 @@ const controlSearchResults = async function () {
     //2) Load search results
     await model.loadSearchResults(query);
 
-    //Render results
-    console.log(model.state.search.results);
+    //3)Render results
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
 };
-controlSearchResults();
 
 const init = function () {
   recipeView.addHandlerREnder(controlRecipes);
