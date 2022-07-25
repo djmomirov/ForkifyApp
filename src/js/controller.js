cc80b0d6b097932dff2672c7c41f1bd5768e0b22
djordje <dj.momirov@gmail.com>
2022-07-25 11:24:22 +0200

    Implemented search option

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,17 +3,17 @@ import recipeView from './views/recipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import searchView from './views/searchView.js';
 
-const recipeContainer = document.querySelector('.recipe');
+// const recipeContainer = document.querySelector('.recipe');
 
-// https://forkify-api.herokuapp.com/v2
+// // https://forkify-api.herokuapp.com/v2
 
-///////////////////////////////////////
+// ///////////////////////////////////////
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -28,7 +28,27 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    //1)Get search query
+    const query = searchView.getQuery();
+
+    if (!query) return;
+
+    //2) Load search results
+    await model.loadSearchResults(query);
+
+    //Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+controlSearchResults();
+
 const init = function () {
   recipeView.addHandlerREnder(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
+
 init();
