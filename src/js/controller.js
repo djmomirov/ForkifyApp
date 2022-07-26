831dccd3b6bdb1cb3099a3269f221b8d003121f6
djordje <dj.momirov@gmail.com>
2022-07-26 13:13:51 +0200

    Updated recipe servings buttons

--- src/js/controller.js
+++ src/js/controller.js
@@ -59,8 +59,17 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+  //Update the recipe servings (ins state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerREnder(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
